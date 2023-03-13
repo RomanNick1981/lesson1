4bb4af1b47020c2437a9d34348f393ee52c9ec72
RomanNick1981 <125348869+RomanNick1981@users.noreply.github.com>
2023-03-13 15:39:11 +0700

    Загрузил Task4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿// Задание 2 
-// Console.Clear();
-// Console.Write("Введите 2 числа для сравнения:\n ");
-// Console.Write("Введите 1-е число для сравнения: ");
-// int a = Convert.ToInt32(Console.ReadLine()!);
-// Console.Write("Введите 2-е число для сравнения: ");
-// int b = Convert.ToInt32(Console.ReadLine()!);
-// if (a > b) a=max; 
-//     b=min;
-// else b=max;
-//     a=min;
-// Console.Write("Результат число " + max + " - Максимальное\n ");
-// Console.Write("Результат число " + min + " - Минимальное\n ");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.Clear();
+Console.Write("Введите 3 числа для сравнения:\n ");
+Console.Write("Введите 1-е число для сравнения: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите 2-е число для сравнения: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите 3-е число для сравнения: ");
+int c = Convert.ToInt32(Console.ReadLine()!);
+if (a > b) b=a;
+if (b>c) c=b;
+Console.Write("Результат число " + c + " - Максимальное\n ");
